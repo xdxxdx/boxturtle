dd0e8fd0b3ecafecd3bd6ab9d6535998a865cf74
xdx <2717814812@qq.com>
2019-10-17 20:47:46 +0800

    重新生成boxturtle-admin

--- boxturtle-admin/src/main.js
+++ boxturtle-admin/src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
-import './plugins/element.js'
 
 Vue.config.productionTip = false
 
 new Vue({
-  store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
